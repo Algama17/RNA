f083206d9c6b17e9bc9f900437c65b566a60a8e9
Algama17 <albertogarciamallada@gmail.com>
2025-03-31 00:51:58 +0200

    code refactoring

--- src/main/java/dev/alberto/RNA_Transcription.java
+++ src/main/java/dev/alberto/RNA_Transcription.java
@@ -3,20 +3,16 @@ package dev.alberto;
 public class RNA_Transcription {
 
     public String transcribe(String dna){
-        if (dna.equals("G")) {
-            return "C";
-            
-        }else if (dna.equals("C")) {
-            return "G";
-        
-        }else if (dna.equals("T")) {
-            return "A";
-        }
-        else if (dna.equals("A")) {
-            return "U";
-        }
-
-        
-        return "";
+        StringBuilder rna = new StringBuilder();
+        for(char c : dna.toCharArray()){
+            switch (c) {
+                case 'G': rna.append('C'); break;
+                case 'C': rna.append('G'); break;
+                case 'T': rna.append('A'); break;
+                case 'A': rna.append('U'); break;
+                default: throw new IllegalArgumentException("Nucleótido inválido: " + c);
+            }
+    }
+        return rna.toString();
     }
 }
