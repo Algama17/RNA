24f1aa440d641ad224820be56b991ece72a3b6b0
Algama17 <albertogarciamallada@gmail.com>
2025-03-31 00:38:15 +0200

    fourth code

--- src/main/java/dev/alberto/RNA_Transcription.java
+++ src/main/java/dev/alberto/RNA_Transcription.java
@@ -9,7 +9,11 @@ public class RNA_Transcription {
         }else if (dna.equals("C")) {
             return "G";
         
+        }else if (dna.equals("T")) {
+            return "A";
         }
+
+        
         return "";
     }
 }
